04e46d8bf3d870ea89c61d443f6dd302c11df42b
Thomas Johnson <tjohnson1106@gmail.com>
2019-10-22 15:41:09 -0400

    created address validator

---
+++ src/Validation/AddressValidation.ts
@@ -0,0 +1,27 @@
+import { MinLengthValidator } from "../Validators/MinLengthValidator";
+import { RegularExpressionValidator } from "../Validators/RegularExpressionValidator";
+import { IPersonState } from "../State";
+
+export class AddressValidation implements IValidation {
+  private readonly minLengthValidator: MinLengthValidator = new MinLengthValidator(
+    5
+  );
+  private readonly zipCodeValidator: RegularExpressionValidator = new RegularExpressionValidator(
+    "^[0-9]{5}(?:-[0-9]{4})?$"
+  );
+
+  public Validate(state: IPersonState, errors: string[]): void {
+    if (!this.minLengthValidator.IsValid(state.Address1)) {
+      errors.push("Address line must be greater than 5 characters");
+    }
+    if (!this.minLengthValidator.IsValid(state.Town)) {
+      errors.push("Town must be greater than 5 characters");
+    }
+    if (!this.minLengthValidator.IsValid(state.County)) {
+      errors.push("County must be greater than 5 characters");
+    }
+    if (!this.zipCodeValidator.IsValid(state.PostCode)) {
+      errors.push("Postal/zip must be greater than 5 characters");
+    }
+  }
+}
